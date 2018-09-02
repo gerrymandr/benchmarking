ac69b85ae1b1c4ef2f0a7e0c267699c3c409563d
Sloan Nietert <sloan.nietert@gmail.com>
2018-09-02 14:39:22 +0200

    added more detail to comments in main script

--- plots/main.py
+++ plots/main.py
@@ -2,6 +2,8 @@
 """
 Created on Tue Jul 24 14:56:26 2018
 @author: Sloan
+
+This is the main script for producing benchmark plots, divided into labeled sections. Several sampled ensembles are compared to a fully enumerated ensemble according to a chosen feature.
 """
 import numpy as np
 import pickle
@@ -21,6 +23,10 @@ sampled_ensemble_paths = {
 feature = entropy
 feature_name = 'Partition Entropy'
 feature_axis_label = 'Entropy'
+# the following settings are really the only manual part of the plotting process
+# feature_hist_bins is the bins argument for the histogram function, fiddle around with this
+# iterations_step is the number of iterations skipped between points plotted on running error and exploration charts
+# (computing at each iteration is unnecessary and slow)
 feature_hist_bins = np.linspace(-75,-50,15)
 iterations_step = 100000
 
